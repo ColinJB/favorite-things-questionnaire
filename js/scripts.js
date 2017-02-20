2bbc05357050d702ecb514094e86f7cf552ef04e
Colin Bloom <colinjbloom113@gmail.com>
2017-02-20 15:16:53 -0800

    change format to grocery list, sorts to alphabetical and uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,21 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
 
-      var food = $("input#fav1").val();
-      var animal = $("input#fav2").val();
-      var sport = $("input#fav3").val();
-      var holiday = $("input#fav4").val();
+      var list1 = $("input#list1").val();
+      var list2 = $("input#list2").val();
+      var list3 = $("input#list3").val();
+      var list4 = $("input#list4").val();
 
-      var array1 = [food, animal, sport, holiday];
-      var array2 = [array1[1], array1[0], array1[2]];
+      var items = [list1, list2, list3, list4];
+      var sortItems = items.map(function(item) {
+        return item.toUpperCase();
+      })
+      sortItems.sort();
+
+      $(".list1").text(sortItems[0]);
+      $(".list2").text(sortItems[1]);
+      $(".list3").text(sortItems[2]);
+      $(".list4").text(sortItems[3]);
 
       $("#output").show();
 
