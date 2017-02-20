f5be67e35a4c7f1e1d2f3c177227d527db584e9b
Colin Bloom <colinjbloom113@gmail.com>
2017-02-20 11:33:20 -0800

    create arrays and show output list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-
-  var food = $("input#fav1").val();
-  var animal = $("input#fav2").val();
-  var sport = $("input#fav3").val();
-  var holiday = $("input#fav4").val();
-
   $("#formOne").submit(function(event) {
 
+
+      var food = $("input#fav1").val();
+      var animal = $("input#fav2").val();
+      var sport = $("input#fav3").val();
+      var holiday = $("input#fav4").val();
+
+      var array1 = [food, animal, sport, holiday];
+      var array2 = [array1[1], array1[0], array1[2]];
+
+      $("#output").show();
+
+
     event.preventDefault();
-  })
+  });
 });
